d09794abd827ea44a3bbe9f9ba453fbbc22efb18
Chao Zhang <raidenchao@gmail.com>
2020-10-02 12:56:03 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Stepper from './Stepper'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+const theme = createMuiTheme({
+  // palette: {
+  //   primary: {
+  //     main: '#21C3F1',
+  //   },
+  //   secondary: {
+  //     main: '#366283',
+  //   },
+  // },
+});
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <Stepper />
     </div>
   );
 }
